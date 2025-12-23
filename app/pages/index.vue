0bea6b483cb58491f862baed17e6c57a91cffb03
Jairzijp <jairzijp123@gmail.com>
2025-12-23 11:36:29 +0100

    change text proposal notice

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,11 +4,9 @@
     <div class="notification is-warning is-light">
       <p>
         <strong>Notice:</strong>
-        There is an active Nosana Network Proposal (NNP-0001) to better retarget emissions for rewards. If it passes,
-        passive staking rewards
-        will sunset over a 12-month period. Read more
-        <a href="https://github.com/nosana-ci/network-proposals/blob/main/nnp/NNP-0001-tokenomics.md" target="_blank"
-          rel="noopener">here</a>.
+        Nosana Network Proposal (NNP-0001) to better retarget emissions for rewards has passed. Passive staking
+        rewards will sunset over a 12-month period. Read more
+        <a href="https://vote.nosana.com/" target="_blank" rel="noopener">here</a>.
       </p>
     </div>
     <div class="columns is-multiline">
